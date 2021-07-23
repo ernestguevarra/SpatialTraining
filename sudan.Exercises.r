7f7ca4e6b87e4bc2e866b450e2cc94b932b7a85e
Emmanuel Mandalazi <emandalazi@yahoo.co.uk>
2021-07-23 15:08:19 +0200

    hi

--- sudan.Exercises.r
+++ sudan.Exercises.r
@@ -11,7 +11,6 @@ coverage <- c(95, 56, 73, 55, 19, 97, 60, 2, 46, 31, 70, 23, 71, 66, 38, 78, 74,
 sudan01 <- cbind(sudan01, coverage = coverage)
 names(sudan01)[ncol(sudan01)] <- "coverage"
 
-<<<<<<< HEAD
 ## Create a plot of the coverage values per State
 
 ## view the structure of the data
@@ -39,30 +38,22 @@ plot(sudan01)
 
 ### Load library RColorBrewer
 #install.packages("RColorBrewer")
-=======
+
 sudan01@data
 
 ## Create a plot of the coverage values per State
 plot(sudan01)
 
 ##choose the colour scheme 
->>>>>>> d6c3583ef1d8b8dede05906e59aa8400d9814b31
 
 library(RColorBrewer)
 
 ## Get RdYlGn colours using RColorBrewer
 RdYlGn <- brewer.pal(n = 5, name = "RdYlGn")
 
-<<<<<<< HEAD
-## merge Coverage Data with Sudan map
-
-sudan01 <- merge(x = sudan01, y = coverage, 
-                   by.x = "State_En", by.y = "State_En")
-
-
 ## Classify coverage values using the cut function and command
 coverage_class <- cut(x = sudan01@data$coverage,
-                    breaks = c(0, 0.25, 0.5, 0.75, 1),
+                    breaks = c(0, 25, 50, 75, 100),
                     include.lowest = FALSE, right = TRUE, label = FALSE) + 1
 coverage_class <- ifelse(is.na(coverage_class), 1, coverage_class)
 
@@ -71,7 +62,7 @@ coverage_class <- ifelse(is.na(coverage_class), 1, coverage_class)
 plot(sudan01,
      col = RdYlGn[coverage_class], 
      border = "gray50", lwd = 2)
-text(coordinates(sudan01), labels = sudan01@data$State_En)
+text(coordinates(sudan01), labels = sudan01@data$State_En, cex = 0.8)
 
 ## Add a legend
 
@@ -88,23 +79,29 @@ legend(
 ## Mapping classifications rather than numbers
 RdYlGn <- brewer.pal(n = 3, name = "RdYlGn")
 
-plot(westPokot,
-     col = ifelse(sudan01t@data$coverage_class == "low", RdYlGn[1],
+coverage_class <- cut(x = sudan01@data$coverage,
+                      breaks = c(0, 30, 70, 100),
+
+                                          include.lowest = TRUE, right = FALSE, label = c("low", "moderate", "high"))
+sudan01 <- cbind(sudan01, coverage_class = coverage_class)
+
+names(sudan01)[ncol(sudan01)] <- "coverage_class"
+plot(sudan01,
+     col = ifelse(sudan01@data$coverage_class == "low", RdYlGn[1],
                   ifelse(sudan01@data$coverage_class == "moderate", RdYlGn[2], RdYlGn[3])), 
      border = "gray50", lwd = 3)
+
 text(coordinates(sudan01), labels = sudan01@data$State_En)
 
 ## Add a map legend to the Vitamin A Coverage classification
 legend(
   title = "Vitamin Coverage Classification",
   x = "topright", inset = 0.002,
-  legend = c("Low (0-20%)", "Moderate (20%-50%)", "High (50%-100%"),
+  legend = c("Low (0-30%)", "Moderate (30%-70%)", "High (70%-100%"),
   pch = 22, pt.cex = 2,
   col = RdYlGn, pt.bg = RdYlGn,
   bty = "n", cex = 0.75
 )
-=======
 
->>>>>>> d6c3583ef1d8b8dede05906e59aa8400d9814b31
 
 
